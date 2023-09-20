9c2053af408247d50ae48bfd9e7a977c992bcc33
lololomick <lololomick@gmail.com>
2023-09-20 09:26:16 +0200

    update

--- index.php
+++ index.php
@@ -8,24 +8,28 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/header.css" type="text/css">
     <script src="https://kit.fontawesome.com/5bb7bb3e3a.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <title>Project 2</title>
 </head>
 <body>
     <main>
-        <header class="header">
-            <a id="homeimg" href="#home"><img id="homelogo" src="img/logo.png" alt="pvv logo"></a>
-            <a href="#home" class="active"><i class="fa fa-home"></i> Home</a>
-            <a href="#about"><i class="fa fa-user"></i> Over ons</a>
-            <a href="#projects"><i class="fa fa-list"></i> Nieuws</a>
-            <a href="#contact"><i class="fa fa-donate"></i> Doneren</a>
-            <a href="#contact"><i class="fa fa-comments"></i> Contact</a>
+    <header class="header sticky sticky--top js-header">
+        <div class="grid">
+          <nav class="navigation">
+            <ul class="list list_inline">
+              <li class="nav_item"><a href="index.php" class="nav_link img_nav"><img class="nav_img" src="img/logo.png" alt="logo"></a></li>
+              <li class="nav_item"><a href="index.php" class="nav_link nav_link_active">Home</a></li>
+              <li class="nav_item"><a href="bestuur.php" class="nav_link">Over ons</a></li>
+              <li class="nav_item"><a href="nieuws.php" class="nav_link">Nieuws</a></li>
+              <li class="nav_item"><a href="doneren.php" class="nav_link">Doneren</a></li>
+              <li class="nav_item"><a href="contact.php" class="nav_link">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
         </header>
         <section>
-            <div id="imageholder">
-                <img id="homeimg1" src="img\nieuws\splash__photo--2.jpg" alt="">
-            </div>
         </section>
         <footer>
             <div class="footer">
