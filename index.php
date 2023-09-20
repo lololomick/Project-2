0df3ea03d165748828caedcc29463369757cb062
xanil1 <125271645+xanil1@users.noreply.github.com>
2023-09-20 09:26:03 +0200

    footer done

--- index.php
+++ index.php
@@ -16,11 +16,11 @@ session_start();
     <main>
         <header class="header">
             <a id="homeimg" href="#home"><img id="homelogo" src="img/logo.png" alt="pvv logo"></a>
-            <a href="#home" class="active"><i class="fa fa-home"></i> Home</a>
-            <a href="#about"><i class="fa fa-user"></i> Over ons</a>
-            <a href="#projects"><i class="fa fa-list"></i> Nieuws</a>
-            <a href="#contact"><i class="fa fa-donate"></i> Doneren</a>
-            <a href="#contact"><i class="fa fa-comments"></i> Contact</a>
+            <a href="index.php" class="active"><i class="fa fa-home"></i> Home</a>
+            <a href="bestuur.php"><i class="fa fa-user"></i> Over ons</a>
+            <a href="nieuws.php"><i class="fa fa-list"></i> Nieuws</a>
+            <a href="doneren.php"><i class="fa fa-donate"></i> Doneren</a>
+            <a href="contact.php"><i class="fa fa-comments"></i> Contact</a>
         </header>
         <section>
             <div id="imageholder">
@@ -28,9 +28,11 @@ session_start();
             </div>
         </section>
         <footer>
-            <div class="footer">
-                <p>footer</p>
+        <footer>
+            <div class="cr-con"><a class="footer1" href="bestuur.php" target="_blank">Over ons |</a> <a class="footer1" href="contact.php" target="_blank">contact info |</a>
+              <a class="footer1">Email |</a> <a class="footer1" href="https://www.Facebook.com/" target="_blank">Facebook </a> <div class='cr-con'> KVK:88599665 | BTW NL999888492Z09 |  &copy; <script>document.write(new Date().getFullYear());</script> </div>
             </div>
+      </footer>
         </footer>
     </main>
 </body>
