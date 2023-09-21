8ec22d0bfc666ea819102a40e8cbc9e199a229a9
xanil1 <125271645+xanil1@users.noreply.github.com>
2023-09-21 11:18:07 +0200

    up

--- doneren.php
+++ doneren.php
@@ -6,7 +6,7 @@
       <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
       <link rel="stylesheet" href="css/style.css" type="text/css">
       <link rel="stylesheet" href="css/header.css" type="text/css">
-      <link rel="stylesheet" href="css/bestuur.css" type="text/css">
+      <link rel="stylesheet" href="css/doneren.css" type="text/css">
       <script src="https://kit.fontawesome.com/5bb7bb3e3a.js" crossorigin="anonymous"></script>
       <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
       <title>Project 2</title>
@@ -17,10 +17,10 @@
           <nav class="navigation">
             <ul class="list list_inline">
               <li class="nav_item"><a href="index.php" class="nav_link img_nav"><img class="nav_img" src="img/logo.png" alt="logo"></a></li>
-              <li class="nav_item"><a href="index.php" class="nav_link nav_link_active">Home</a></li>
+              <li class="nav_item"><a href="index.php" class="nav_link">Home</a></li>
               <li class="nav_item"><a href="bestuur.php" class="nav_link">Over ons</a></li>
               <li class="nav_item"><a href="nieuws.php" class="nav_link">Nieuws</a></li>
-              <li class="nav_item"><a href="doneren.php" class="nav_link">Doneren</a></li>
+              <li class="nav_item"><a href="doneren.php" class="nav_link nav_link_active">Doneren</a></li>
               <li class="nav_item"><a href="contact.php" class="nav_link">Contact</a></li>
             </ul>
           </nav>
@@ -28,7 +28,20 @@
     </header>
     <main>
       <section>
-        
+        <div class="container">
+          <div class="title"><img class="image" src="img/dies2048xfinal_21.jpg" alt=""></div>
+          <div class="Doneren">Doneren</div>
+          <div class="persoonlijk-gevenens">persoonlijk gegevens</div>
+          <div class="naam">naam</div>
+          <div class="achernaam">achternaam</div>
+          <div class="geboortedatum">geboortedatum</div>
+          <div class="leeg">.</div>
+          <div class="contact-gegevens">contact gegevens</div>
+          <div class="email">email</div>
+          <div class="telefoonnummer">telefoonnummer</div>
+          <div class="Donatie">donatie</div>
+          <div class="donatie-geld">donatie geld</div>
+        </div>
       </section>
     </main>
     <footer>
